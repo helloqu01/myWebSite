24ae6f349295adc728920b2efdf873c7417ae9f4
oh hyunji <ddaaadd01@gmail.com>
2025-07-17 11:49:49 +0900

    람다 파일 수정

--- back/src/lambda.ts
+++ back/src/lambda.ts
@@ -3,31 +3,32 @@ import { Handler, Context, APIGatewayProxyEvent, Callback } from 'aws-lambda';
 import { NestFactory } from '@nestjs/core';
 import { ExpressAdapter } from '@nestjs/platform-express';
 import * as express from 'express';
-import serverlessExpress from '@vendia/serverless-express';
+import * as awsServerlessExpress from 'aws-serverless-express';
 import { AppModule } from './app.module';
 
 let server: Handler;
 
 async function bootstrap() {
-  const app = express();
+  const expressApp = express();
   const nestApp = await NestFactory.create(
     AppModule,
-    new ExpressAdapter(app),
+    new ExpressAdapter(expressApp),
   );
   nestApp.setGlobalPrefix('api');
   await nestApp.init();
-  server = serverlessExpress({ app });
+
+  // aws-serverless-express로 래핑
+  const awsServer = awsServerlessExpress.createServer(expressApp);
+  server = (event: APIGatewayProxyEvent, ctx: Context, cb: Callback) =>
+    awsServerlessExpress.proxy(awsServer, event, ctx, 'PROMISE').promise;
 }
 
+// 앱 초기화
 bootstrap();
 
-export const handler: Handler = (
-  event: APIGatewayProxyEvent,
-  ctx: Context,
-  cb: Callback,
-) => {
+export const handler: Handler = (event, context, callback) => {
   if (!server) {
-    return cb(new Error('Server not initialized'), undefined);
+    return callback(new Error('Server not initialized'), undefined);
   }
-  return server(event, ctx, cb);
+  return server(event, context, callback);
 };
