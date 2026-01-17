7e3b6446023ef1dec1400db04dbc8e631800afd3
Oh Hyunji <helloqu@naver.com>
2026-01-17 17:54:44 +0900

    태그관리자 추가

--- front/src/app/head.tsx
+++ front/src/app/head.tsx
@@ -5,19 +5,6 @@ export default function Head() {
       <title>Oh Hyunji – FullStack Web Developer</title>
       <meta name="description" content="Hyunji Oh 의 포트폴리오 사이트입니다." />
 
-      {/* Google Tag Manager */}
-      <script
-        dangerouslySetInnerHTML={{
-          __html:
-            "(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':" +
-            "new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0]," +
-            "j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=" +
-            "'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);" +
-            "})(window,document,'script','dataLayer','GTM-WSMNHKPD');",
-        }}
-      />
-      {/* End Google Tag Manager */}
-
       {/* Open Graph */}
       <meta property="og:type" content="website" />
       <meta property="og:url" content="https://codingbyohj.com" />
